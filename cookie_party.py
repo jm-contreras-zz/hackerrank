0026cbb6f93af2b8af6e8aaf08653999aae803d2
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-08-08 20:36:08 -0400

    Update cookie_party.py

--- cookie_party.py
+++ cookie_party.py
@@ -1,13 +1,16 @@
 #!/bin/python
 
+# Import modules
 import sys
 import math
 
+# Receive standard input
+guests, cookies = raw_input().strip().split(' ')
+guests, cookies = [int(n), int(m)]
 
-n, m = raw_input().strip().split(' ')
-n, m = [int(n), int(m)]
-
-if n >= m:
-    print n - m
+# If guests outnumber or equal cookies, bake the difference
+if guests >= cookies:
+    print guests - cookies
+# Otherwise, find the difference to the nearest multiple and bake as many cookies
 else:
-    print int((math.ceil(m / float(n)) * n) - m)
+    print int((math.ceil(cookies / float(n)) * guests) - cookies)
