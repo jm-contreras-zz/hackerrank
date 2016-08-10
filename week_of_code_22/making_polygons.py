c73dac6634a07fb45c818ee6fb4259648dbb85ce
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-08-09 21:27:17 -0400

    Update making_polygons.py

--- week_of_code_22/making_polygons.py
+++ week_of_code_22/making_polygons.py
@@ -1,5 +1,26 @@
 #!/bin/python
 
+'''
+A polygon is a closed shape with three or more sides. For example, triangles are polygons. A polygon is non-degenerate if it has no
+overlapping sides (and no sides of zero length).
+
+You have n sticks with positive integer lengths, a0, a1, ..., an-1. You want to create a polygon using all n sticks. Because this is
+not always possible, you can cut one or more sticks into two smaller sticks (they do not necessarily need to be of integer length) and
+repeat the process of trying to create a polygon using all the sticks. Given the lengths of all n sticks, find and print the minimum
+number of cuts necessary to make a non-degenerate polygon.
+
+Input Format
+The first line contains a single integer, n. 
+The second line contains  space-separated integers describing the respective values of a0, a1, ..., an-1.
+
+Constraints
+1 <= n <= 50
+1 <= ai <= 100
+
+Output Format
+Print a single integer denoting the minimum number of cuts required to make the n sticks into a polygon.
+'''
+
 # Import module
 import sys
 
