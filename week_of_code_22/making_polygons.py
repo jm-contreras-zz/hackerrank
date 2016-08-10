33269274df08a9e83f6f4b979b54ecb3d2c05917
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2016-08-09 21:23:59 -0400

    Update making_polygons.py

--- week_of_code_22/making_polygons.py
+++ week_of_code_22/making_polygons.py
@@ -1,21 +1,25 @@
 #!/bin/python
 
+# Import module
 import sys
 
-
+# Receive standard input
 n = int(raw_input().strip())
 a = map(int, raw_input().strip().split(' '))
 
-if n == 1:
-    print 2
-elif n == 2:
-    print 1
+# Fewer than 3 sticks
+if n < 3:
+    # Make 1 cut for each missing side of a triangle
+    print 3 - n
+    
+# 3 or more sticks
 else:
-    x = 0
-    for i, this_a in enumerate(a):
-        if this_a < sum(a) / float(2):
-            x += 1
-    if x == n:
-        print 0
-    else:
-        print 1
+    # Count of sticks that meet triangle inequality condition
+    count = 0
+    # Iterate through sticks
+    for i, i_stick in enumerate(a):
+        # Add to count if condition met
+        if i_stick < sum(a) / float(2):
+            count += 1
+    # Make cut if not all sticks meet condition
+    print int(count != n)
